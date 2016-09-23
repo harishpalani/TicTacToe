ee3f0d0a070b641a8510840ee32df1e3df29bb9a
Harish Palani <355131@beaverton.k12.or.us>
2016-09-23 14:28:02 -0700

    framework added

--- ttt.cpp
+++ ttt.cpp
@@ -2,6 +2,45 @@
 
 using namespace std;
 
+#define EMPTY 0;
+#define X 1;
+#define O 2;
+#define X_TURN 0;
+#define O_TURN 1;
+
+void printBoard(int** board) {
+  cout << " \t1\t2\t3";
+  for (int row = 0; row < (sizeof(board) / sizeof(board[0]); row++) {
+    String output = (char) ('a' + row) + "\t";
+    for (int column = 0; column < (sizeof(board[0] / sizeof(board[0][0]); column++) {
+      if (board[row][column] == 0) {
+	output += " \t";
+      } else if (board[row][column] == 1) {
+	output += "X\t";
+      } else if (board[row][column] == 2) {
+	output += "O\t";
+      }
+    }
+    cout << output;
+  }
+}
+
+void playGame() {
+
+}
+
 int main() {
-  cout << "Print stuff!";
+  int board[3][3];
+  int*[3] boardPtr = board;
+  
+  int turn = X_TURN;
+  int xWins = 0;
+  int ties = 0;
+  int gameCount = 0;
+
+  cout << "Tic Tac Toe - three in a row!\n";
+  gameCount++;
+  cout << "Game #" << gameCount;
+
+  playGame();
 }
